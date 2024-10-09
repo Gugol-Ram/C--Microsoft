29bdadbb81f0ce696d3bb6f4d55f9eec37f80464
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-10-09 19:32:22 -0300

    incr & decr ops

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
+/*
+//Incremento y decremento
 int value = 1;
 
 value = value + 1;
@@ -20,3 +21,11 @@ Console.WriteLine("Second decrement: " + value);
 
 value--;
 Console.WriteLine("Third decrement: " + value);
+*/
+
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");// Se recupera el valor de la variable value (actualmente 2) y se usa en la operación de interpolación de cadenas.LUEGO se incrementa su valor.
+Console.WriteLine("Third: " + value);//por lo que aqui value ahora vale 3
+Console.WriteLine("Fourth: " + (++value));//al estar antes de 'value' en este caso primero se incrementa dicho valor. el parentesis no es estrictamente necesario.
